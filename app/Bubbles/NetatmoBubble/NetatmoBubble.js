3d205522a089dd00279db8fe790e50ecdaa90a2d
Juhani Aavanen <ja222qm@student.lnu.se>
2015-02-12 09:17:17 +0200

    Added functions for devices in Netatmo Bubble

--- app/Bubbles/NetatmoBubble/NetatmoBubble.js
+++ app/Bubbles/NetatmoBubble/NetatmoBubble.js
@@ -1,4 +1,5 @@
 var http = require('http');
+
 exports.getDevices = function(req, callback) {
 
   // make request to netatmo API with token
@@ -20,5 +21,99 @@ exports.getDevices = function(req, callback) {
   }).on("error", function(e){
     console.log("Got error: " + e.message);
     callback(new Error("Got error: " + e.message));
+  });
+};
+
+exports.getModule = function(req, callback) {
+
+  var scale = "max";
+  var dateEnd = "last";
+  var type = "Temperature, CO2, Humidity, Pressure, Noise"
+  var params = req.token + "&device_id=" + req.deviceId + "&type=" + type + "&scale=" + scale + "&date_end=" + dateEnd;
+
+  netatmoDevice(params, function(err, info){
+    if(err) {
+      callback(err);
+    }
+    else {
+      callback(null, info);
+    }
+  });
+
+};
+
+exports.getRainGauge = function(req, callback) {
+
+  var scale = "max";
+  var dateEnd = "last";
+  var type = "Rain"
+  var params = req.token + "&device_id=" + req.deviceId + "&type=" + type + "&scale=" + scale + "&date_end=" + dateEnd;
+
+  netatmoDevice(params, function(err, info){
+    if(err) {
+      callback(err);
+    }
+    else {
+      callback(null, info);
+    }
+  });
+
+};
+
+exports.getStation = function(req, callback) {
+
+  var scale = "max";
+  var dateEnd = "last";
+  var type = "Temperature, CO2, Humidity, Pressure, Noise"
+  var params = req.token + "&device_id=" + req.deviceId + "&type=" + type + "&scale=" + scale + "&date_end=" + dateEnd;
+
+  netatmoDevice(params, function(err, info){
+    if(err) {
+      callback(err);
+    }
+    else {
+      callback(null, info);
+    }
+  });
+};
+
+exports.getThermostate = function(req, callback) {
+
+  var scale = "max";
+  var dateEnd = "last";
+  var type = "Temperature, Sp_Temperature"
+  var params = req.token + "&device_id=" + req.deviceId + "&type=" + type + "&scale=" + scale + "&date_end=" + dateEnd;
+
+  netatmoDevice(params, function(err, info){
+    if(err) {
+      callback(err);
+    }
+    else {
+      callback(null, info);
+    }
+  });
+
+};
+
+function netatmoDevice(pathParams, next) {
+
+  var options = {
+    host: 'api.netatmo.net',
+    path: '/api/getmeasure?access_token=' + pathParams
+  };
+
+  http.get(options, function(resp){
+    var str = "";
+    resp.on('data', function(chunk){
+      str += chunk;
+    });
+
+    resp.on('end', function () {
+      next(null, str);
+    });
+
+  }).on("error", function(e){
+    console.log("Got error: " + e.message);
+    next(new Error("Got error: " + e.message));
   });
 };
