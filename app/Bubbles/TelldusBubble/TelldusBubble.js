d2df7a5d7af37f4d3340c3c870b69fa12b83f581
lr222gw <lr222gw@student.lnu.se>
2015-02-18 19:52:14 +0100

    getUser for Netatmo = DONE

--- app/Bubbles/TelldusBubble/TelldusBubble.js
+++ app/Bubbles/TelldusBubble/TelldusBubble.js
@@ -2,3 +2,9 @@ exports.getDevices = function(req, callback) {
 
   callback(null, "not yet implemented");
 };
+
+exports.getUser = function(req, callback){
+
+    callback(null, "not yet implemented");
+
+};
