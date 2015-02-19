2216ae599bd5f3232cd5b8a47bf7270d258160c7
vl222cu <vl222cu@student.lnu.se>
2015-02-19 21:18:10 +0100

    Added more tests to the modules

--- app/Tests/Netatmo/RainGauge.test.js
+++ app/Tests/Netatmo/RainGauge.test.js
@@ -3,14 +3,15 @@
 var superagent = require('superagent');
 var expect = require('expect.js');
 
-describe('Iris netatmo GET weatherStation - need valid deviceId for all tests to be valid', function(){
+describe('Iris netatmo GET rainGauge - need valid deviceId and moduleId for all tests to be valid', function(){
   // Change this access token to a valid one before running the tests
   var access = '52d40bac1877595a67f62431|6467bc65536a0227b1ebb904adf273bb';
   var brand = "Netatmo";
-  var deviceId = "70:ee:50:01:ed:f0";
+  var deviceId = "123";
+  var moduleId = "456";
 
-  it('no access to weatherStation with NO TOKEN', function(done){
-    superagent.get('http://localhost:3000/api/Device/WeatherStation?deviceId=' + deviceId)
+  it('no access to rainGauge with NO TOKEN', function(done){
+    superagent.get('http://localhost:3000/api/Device/RainGauge?deviceId=' + deviceId + '&moduleId=' + moduleId)
       .set('brand', brand)
       .end(function(e, res){
         expect(e).to.not.be.ok();
@@ -19,9 +20,9 @@ describe('Iris netatmo GET weatherStation - need valid deviceId for all tests to
       })
   })
 
-  // Need valid deviceId to test this properly
-  it('no access to weatherStation with BAD TOKEN', function(done){
-    superagent.get('http://localhost:3000/api/Device/WeatherStation?deviceId=' + deviceId)
+  // Need valid deviceId and moduleId to test this properly
+  it('no access to rainGauge with BAD TOKEN', function(done){
+    superagent.get('http://localhost:3000/api/Device/RainGauge?deviceId=' + deviceId + '&moduleId=' + moduleId)
       .set('access_token', "123")
       .set('brand', brand)
       .end(function(e, res){
@@ -31,8 +32,8 @@ describe('Iris netatmo GET weatherStation - need valid deviceId for all tests to
       })
   })
 
-  it('no access to weatherStation NO BRAND', function(done){
-    superagent.get('http://localhost:3000/api/Device/WeatherStation?deviceId=' + deviceId)
+  it('no access to rainGauge NO BRAND', function(done){
+    superagent.get('http://localhost:3000/api/Device/RainGauge?deviceId=' + deviceId + '&moduleId=' + moduleId)
       .set('access_token', access)
       .end(function(e, res){
         expect(e).to.not.be.ok();
@@ -41,8 +42,8 @@ describe('Iris netatmo GET weatherStation - need valid deviceId for all tests to
       })
   })
 
-  it('no access to weatherStation with BAD BRAND', function(done){
-    superagent.get('http://localhost:3000/api/Device/WeatherStation?deviceId=' + deviceId)
+  it('no access to rainGauge with BAD BRAND', function(done){
+    superagent.get('http://localhost:3000/api/Device/RainGauge?deviceId=' + deviceId + '&moduleId=' + moduleId)
       .set('access_token', access)
       .set('brand', 'brand')
       .end(function(e, res){
@@ -52,9 +53,9 @@ describe('Iris netatmo GET weatherStation - need valid deviceId for all tests to
       })
   })
 
-  // Need valid deviceId to test this properly
-  it('retrieves weatherStation with CORRECT HEADERS and QUERY PARAMS', function(done){
-    superagent.get('http://localhost:3000/api/Device/WeatherStation?deviceId=' + deviceId)
+  // Need valid deviceId and moduleId to test this properly
+  it('retrieves rainGauge with CORRECT HEADERS and QUERY PARAMS', function(done){
+    superagent.get('http://localhost:3000/api/Device/RainGauge?deviceId=' + deviceId + '&moduleId=' + moduleId)
       .set('access_token', access)
       .set('brand', brand)
       .end(function(e, res){
