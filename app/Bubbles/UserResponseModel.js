8b6ed706b284cad6aa373ac2ebc0ce6fce664990
lr222gw <lr222gw@student.lnu.se>
2015-03-06 14:55:35 +0100

    UserResponseModel Basis done...

--- app/Bubbles/UserResponseModel.js
+++ app/Bubbles/UserResponseModel.js
@@ -1,16 +1,15 @@
-exports.UserResponseModel = function(){
+exports.UserResponseModel = function(userId,brand,email){
   this.status = 200;
   this.body = {
     user_id : userId,
-    brand : brand,
+    brand   : brand,
+    email   : email
 
   };
-  this.time_exec= time_exec;     //
-  this.time_server= time_server; //
+  this.time_exec= time_exec;
+  this.time_server= time_server;
 
   this.makeJSON = function(){
     return JSON.stringify(this)
-
   }
-
 }
