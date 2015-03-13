097fa5168049af4edb9700b27c2052b9fcb4e8b7
lr222gw <lr222gw@student.lnu.se>
2015-03-13 05:54:15 +0100

    untested version of Telldus getDevices done...

--- app/Bubbles/TelldusBubble/TelldusBubble.js
+++ app/Bubbles/TelldusBubble/TelldusBubble.js
@@ -49,7 +49,27 @@ exports.getDevices = function (req, callback) {
       callback(err);
     }
     else {
-      callback(null, answer);
+
+      var arrOfDevices = JSON.parse(answer);
+      arrOfDevices = arrOfDevices.devices;
+      var arrOfDeviceResponses = [];
+      var obj = {
+        status : 200,
+        devices: null
+      }
+
+      for(var i = 0; i < arrOfDevices.length; i++){
+        var res = response.ResponseModel(arrOfDevices[i].id, arrOfDevices[i].client,null, arrOfDevices[i].name,[],null, null);
+        //Fråga, ska idt från Telldus vara id eller ClientDeviceId ?
+        //Fråga ska mainDevice vara samma som client?  eller är det kanske clientdeviceId?
+        //Vad kan vi ha på deviceType på telldus? det finns ingen indikation på vilken typ devicen är av... Kanske clientDeviceID?
+        //Här kan vi inte direkt skicka med något i Arrayen med meassures då vi itne får ut några meassures av Telldus...
+        arrOfDeviceResponses.push(res.body);
+      }
+
+      obj.devices = arrOfDeviceResponses;
+
+      callback(null, JSON.stringify(obj));
     }
   });
 };
